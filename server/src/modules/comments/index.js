429f0d5f99141600fefccfb510e3b9f0162bf6eb
Andre Coelho <andre.ribeiro@iv2.com.br>
2020-01-06 13:42:09 -0300

    add comments

--- server/src/modules/comments/index.js
+++ server/src/modules/comments/index.js
@@ -5,18 +5,24 @@ import { getComments, saveComment } from "./CommentLoader";
 
 export const mutations = {
   getComments: {
+    // o tipo é uma lista de comentarios
     type: GraphQLList(CommentType),
+    // chama o metodo getCOments
     resolve: getComments
   }
 };
 
 export const queries = {
   saveComment: {
+    // tipo commentario
     type: CommentType,
+    // argumentos de entrada
     args: {
       input: {
         type: new GraphQLInputObjectType({
+          // o nome pode ser qualquer, isso é só para documentação
           name: "CommentInput",
+          // os campos que vai receber
           fields: {
             name: {
               type: GraphQLString
@@ -28,6 +34,7 @@ export const queries = {
         })
       }
     },
+    // chama o metodo de salvar
     resolve: saveComment
   }
 };
